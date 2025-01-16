a91117b7af6f1b37685c778071ef7e64b75af3c0
MIKO SETIAWAN <98937432+mikosetiawan@users.noreply.github.com>
2025-01-16 15:37:44 +0700

    Test udpated
    Test udpated

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,8 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return ['Laravel' => app()->version()];
+    // return ['Laravel' => app()->version()];
+    return "Halo smartcertix";
 });
 
 require __DIR__.'/auth.php';
